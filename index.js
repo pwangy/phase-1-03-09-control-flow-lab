791398b4445b0be35517aca0e997eb436d74deff
pwangy <blipsandclicks@gmail.com>
2024-01-28 19:01:55 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(n){
+  if (n > 2500) {
+    return 'No can do.'
+  } else if (n > 2000 && n < 2501) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (2000 > n && n > 400) {
+    return 'That will be twenty bucks.'
+  } else {
+    return 'This one is on me!'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city  === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
